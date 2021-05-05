c06487a67367d723fde7ea8883ec51861044179c
Shaikh Imran <shaikhimran115@gmail.com>
2021-05-05 18:45:28 +0530

    Update create-generic-tree-using-array.js
    added more methods for generic tree operation like size, height,max,traversal, leverOrder traversal.

--- Generic-Tree/create-generic-tree-using-array.js
+++ Generic-Tree/create-generic-tree-using-array.js
@@ -5,56 +5,93 @@ class TreeNode {
   }
 }
 
-const arr = [10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1];
-
-let stack  = [];
-let root= new TreeNode(null);
-for(let i=0; i<arr.length; i++){
-    if(arr[i] === -1){
-        stack.pop();
-    }else{
-        const t = new TreeNode(arr[i])
-        // t.data = arr[i];
-        if(stack.length > 0){
-            // console.log('-> ',t)
-            stack[stack.length-1].children.push(t);
-        }else{ 
-            root = t;
+const createTree = (arr) =>{
+    let stack = [];
+    let root = new TreeNode(null);
+    arr.forEach((item)=>{
+        if(item === -1){
+            stack.pop();
+        }else{
+            const node = new TreeNode(item);
+            if(stack.length > 0){
+                stack[stack.length-1].children.push(node);
+            }else{
+                root = node;
+            }
+            stack.push(node);
         }
-        stack.push(t)
-    }
-}
-
-console.log(root);
-
-const display = (root) =>{
-    let str = root.data+"=>";
-    let len = root.children.length;
-    root.children.forEach((node,index)=>{
-        let comma = ",";
-        if(len-1 === index){
-            comma = "";
-        }
-        str += node.data+ comma;
-    });
-    // str+=".";
-    console.log(str);
-    root.children.forEach((node)=>{
-        display(node);
     })
-
+    return root;
 }
 
-
-const size = (node) =>{
-    let s = 0;
-    node.children.forEach((item)=>{
-        let cs = size(item)
-        s = s + cs;
+const display= (root)=>{
+    let str = root.data+"=>";
+    root.children.forEach((child)=>{
+        str = str + child.data +", ";
     });
-    s = s +1;
-    return s;
-} 
+    console.log(str);
+    root.children.forEach((child)=>{
+        display(child)
+    });
+}
 
-display(root);
-console.log('Size of a tree : '+size(root));
+const size = (root)=>{
+    let s = 0;
+    root.children.forEach(child=>{
+        const cs = size(child);
+        s = s + cs;
+    })
+    s = s + 1;
+    return s;
+}
+
+const maxVal = (root)=>{
+    let max = Number.MIN_VALUE;
+    root.children.forEach(child=>{
+        const cm = maxVal(child);
+        max = Math.max(cm ,max);
+    })
+    max = Math.max(max, root.data);
+    return max;
+}
+
+const height = (root)=>{
+    let ht = -1;
+    root.children.forEach(child=>{
+        const ch = height(child);
+        ht = Math.max(ch,ht);
+    })
+    ht = ht + 1;
+    return ht;
+}
+
+const traversal = (root)=>{
+    console.log('Node Pre ',root.data);
+    root.children.forEach(child=>{
+        traversal(child);
+    });
+    console.log('Node Post ',root.data);
+}
+
+const levelOrder = (root) =>{
+    const queue = [];
+    queue.unshift(root);
+    let level = "";
+    while(queue.length > 0){
+        root = queue.pop();
+        level = level + root.data+" ";
+        root.children.forEach(child=>{
+            queue.unshift(child);
+        });
+    }
+    console.log(level)
+}
+// const eulerArr = [10,20,-1,30,-1,-1];
+const eulerArr = [10,20,40,-1,50,-1,-1,30,60,-1,70,-1,-1,-1];
+const rootPath = createTree(eulerArr);
+// display(rootPath);
+// console.log(size(rootPath))
+// console.log(maxVal(rootPath))
+//  console.log(height(rootPath))
+// traversal(rootPath);
+levelOrder(rootPath)
